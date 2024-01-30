bcf5ff493c07622ab49bd98886c9e240e6fbfed9
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-30 14:43:13 +0530

    Initial Commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -14,6 +14,26 @@ class LoginPage extends StatelessWidget {
           style: TextStyle(color: Colors.white),
         ),
       ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Email',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Container(height: 30),
+          TextField(
+            decoration: InputDecoration(
+              hintText: 'Password',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          Container(height: 30),
+          ElevatedButton(onPressed: () {}, child: Text('Login'))
+        ],
+      ),
     );
   }
 }
