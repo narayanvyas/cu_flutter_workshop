935333cc8d60bc36c7cddbf29e86fd8c60219024
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-30 15:50:30 +0530

    Initial Commit

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
@@ -17,6 +18,9 @@ class LoginPage extends StatelessWidget {
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 30, vertical: 30),
         children: [
+          Lottie.network(
+              'https://lottie.host/9d5f1eb5-b95b-4360-a3d2-aa21050deb79/ljXdFym0I2.json',
+              height: 200),
           TextField(
             decoration: InputDecoration(
               hintText: 'Email',
