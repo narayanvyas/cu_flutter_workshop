ae2703a4e9c66cf2af8307d4943cf9d0b941de6f
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-30 18:45:16 +0530

    Updated Products API Code

--- lib/products_page.dart
+++ lib/products_page.dart
@@ -24,106 +24,156 @@ class ProductsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        centerTitle: true,
-        actions: [
-          IconButton(
-              onPressed: () {
-                getProducts();
-              },
-              icon: Icon(
-                Icons.electric_car_outlined,
-                color: Colors.white,
-              ))
-        ],
-        title: Text(
-          'Products',
-          style: TextStyle(color: Colors.white),
-        ),
-      ),
-      body: ListView(
-        children: [
-          Card(
-            child: ListTile(
-              title: Image.network(
-                  'https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 10,
-                  ),
-                  Text(
-                    'Product Name',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Container(
-                    height: 10,
-                  ),
-                  Text(
-                      'Casjd hshjxbajshjajhabh adh dsjhvchjsdbchjbds jhc vasdhjv chjds  vcdjhv'),
-                  Container(
-                    height: 10,
-                  ),
-                  Row(
-                    children: [
-                      Text('Category'),
-                      Spacer(),
-                      Text(
-                        '110\$',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            color: Colors.red),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          centerTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  getProducts();
+                },
+                icon: Icon(
+                  Icons.electric_car_outlined,
+                  color: Colors.white,
+                ))
+          ],
+          title: Text(
+            'Products',
+            style: TextStyle(color: Colors.white),
           ),
-          Card(
-            child: ListTile(
-              title: Image.network(
-                  'https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg'),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    height: 10,
-                  ),
-                  Text(
-                    'Product Name',
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                  ),
-                  Container(
-                    height: 10,
-                  ),
-                  Text(
-                      'Casjd hshjxbajshjajhabh adh dsjhvchjsdbchjbds jhc vasdhjv chjds  vcdjhv'),
-                  Container(
-                    height: 10,
-                  ),
-                  Row(
+        ),
+        body: FutureBuilder(
+          future: getProducts(),
+          builder: (context, products) {
+            if (!products.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            return ListView.builder(
+              itemCount: products.data!.length,
+              itemBuilder: (context, index) => Card(
+                child: ListTile(
+                  title: Image.network(products.data![index].image ?? ''),
+                  subtitle: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text('Category'),
-                      Spacer(),
-                      Text(
-                        '110\$',
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 24,
-                            color: Colors.red),
+                      Container(
+                        height: 10,
                       ),
+                      Text(
+                        products.data![index].title ?? '',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      Container(
+                        height: 10,
+                      ),
+                      Text(products.data![index].description ?? ''),
+                      Container(
+                        height: 10,
+                      ),
+                      Row(
+                        children: [
+                          Text(products.data![index].category!.name),
+                          Spacer(),
+                          Text(
+                            (products.data![index].price ?? 0).toString(),
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 24,
+                                color: Colors.red),
+                          ),
+                        ],
+                      )
                     ],
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
-          )
-        ],
-      ),
-    );
+            );
+          },
+        )
+
+        // ListView (
+        //   children: [
+        //     Card(
+        //       child: ListTile(
+        //         title: Image.network(
+        //             'https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg'),
+        //         subtitle: Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Text(
+        //               'Product Name',
+        //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        //             ),
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Text(
+        //                 'Casjd hshjxbajshjajhabh adh dsjhvchjsdbchjbds jhc vasdhjv chjds  vcdjhv'),
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Row(
+        //               children: [
+        //                 Text('Category'),
+        //                 Spacer(),
+        //                 Text(
+        //                   '110\$',
+        //                   style: TextStyle(
+        //                       fontWeight: FontWeight.bold,
+        //                       fontSize: 24,
+        //                       color: Colors.red),
+        //                 ),
+        //               ],
+        //             )
+        //           ],
+        //         ),
+        //       ),
+        //     ),
+        //     Card(
+        //       child: ListTile(
+        //         title: Image.network(
+        //             'https://cdn.pixabay.com/photo/2023/12/15/22/37/mountains-8451480_1280.jpg'),
+        //         subtitle: Column(
+        //           crossAxisAlignment: CrossAxisAlignment.start,
+        //           children: [
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Text(
+        //               'Product Name',
+        //               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+        //             ),
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Text(
+        //                 'Casjd hshjxbajshjajhabh adh dsjhvchjsdbchjbds jhc vasdhjv chjds  vcdjhv'),
+        //             Container(
+        //               height: 10,
+        //             ),
+        //             Row(
+        //               children: [
+        //                 Text('Category'),
+        //                 Spacer(),
+        //                 Text(
+        //                   '110\$',
+        //                   style: TextStyle(
+        //                       fontWeight: FontWeight.bold,
+        //                       fontSize: 24,
+        //                       color: Colors.red),
+        //                 ),
+        //               ],
+        //             )
+        //           ],
+        //         ),
+        //       ),
+        //     )
+        //   ],
+        // ),
+        );
   }
 }
