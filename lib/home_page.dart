e0f425eb577d4e82b21488670e6d817d6a8e2d6a
Narayan Vyas <narayanvyas@Narayans-MacBook-Pro.local>
2024-01-30 12:05:03 +0530

    Initial Commit

---
+++ lib/home_page.dart
@@ -0,0 +1,13 @@
+// To create stateless page, Type - stl
+// To create stateful page, Type - stf
+
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'My First App',
+    home: HomePage(),
+  ));
+}
